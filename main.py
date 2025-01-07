a3c2406b4d104f3bc175e9b63c89711600acf2cf
Ike Maldonado <maldonadoike@gmail.com>
2025-01-07 08:09:13 -0700

    add remove algorithm

--- main.py
+++ main.py
@@ -7,6 +7,63 @@ class BST:
 
 
 
+    def remove(self, key):
+        parent = None
+        current_node = self.root
+        while current_node is not None:
+            if current_node.key == key:
+                if current_node.left is None and current_node.right is None:
+                    # remove leaf
+                    if parent is None:  # Node is root
+                        self.root = None
+                    elif parent.left == current_node:
+                        parent.left = None
+                    else:
+                        parent.right = None
+                    return True  # node found and removed
+                elif current_node.right is None:
+                    # remove node with only left child
+                    if parent is None:  # Node is root
+                        self.root = current_node.left
+                    elif parent.left == current_node:
+                        parent.left = current_node.left
+                    else:
+                        parent.right = current_node.left
+                    return True  # node found and removed
+                elif current_node.left is None:
+                    # remove node with only right child
+                    if parent is None:  # node is root
+                        self.root = current_node.right
+                    elif parent.left == current_node:
+                        parent.left = current_node.right
+                    else:
+                        parent.right = current_node.right
+                    return True  # Node found and removed
+                else:
+                    # remove node with two children
+                    # find successor (leftmost child of right subtree)
+                    successor = current_node.right
+                    while successor.left is not None:
+                        successor = successor.left
+
+                    # copy successor's key to current node
+                    current_node.key = successor.key
+                    parent = current_node
+
+                    # reassign current node and key so that loop continues with new key
+                    current_node = successor.right
+                    key = successor.key
+
+            elif current_node.key > key:
+                # search right
+                parent = current_node
+                current_node = current_node.right
+            else:
+                # search left
+                parent = current_node
+                current_node = current_node.left
+        return False  # node not found
+
     def print_in_order(self, node):
         if node is None:
             return
