bf3c40f2ba7167462e5a7b0077567d486c0e51db
Ike Maldonado <maldonadoike@gmail.com>
2025-01-07 07:59:38 -0700

    add print algorithm

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ class BST:
     def __init__(self):
         self.root = None
 
+
+
+    def print_in_order(self, node):
+        if node is None:
+            return
+        self.print_in_order(node.left)
+        print(node)
+        self.print_in_order(node.right)
+
+    def get_height(self, node):
+        if node is None:
+            return -1
+        left_height = self.get_height(node.left)
+        right_height = self.get_height(node.right)
+        return 1 + max(left_height, right_height)
+
     def insert(self, node):
         if self.root is None:
             self.root = node
