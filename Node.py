1dad039a9df2e8eb01bac8969f22141a8949b78c
Ike Maldonado <maldonadoike@gmail.com>
2025-01-07 07:56:11 -0700

    adjust node

--- Node.py
+++ Node.py
@@ -2,5 +2,5 @@ class Node:
     def __init__(self, key):
         self.left = None
         self.right = None
-        self.value = key
+        self.key = key
         self.parent = None
--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from Node import Node
 
 
-def main() -> None:
-    root = Node(10)
-    root.left = Node(5)
-    root.right = Node(20)
+class BST:
+    def __init__(self):
+        self.root = None
 
-    print(root.value)
-    print(root.left.value)
-    print(root.right.value)
+    def insert(self, node):
+        if self.root is None:
+            self.root = node
+            node.parent = None
+            return
+
+        cur = self.root
+        while cur is not None:
+            if node.key < cur.key:
+                if cur.left is None:
+                    cur.left = node
+                    node.parent = cur
+                    cur = None
+                else:
+                    cur = cur.left
+            else:
+                if cur.right is None:
+                    cur.right = node
+                    node.parent = cur
+                    cur = None
+                else:
+                    cur = cur.right
 
 
 if __name__ == "__main__":
-    main()
+    pass
