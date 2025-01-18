cd4f2d7e551d4090eb67e036ca3ab45bcfac0ed4
Ike Maldonado <maldonadoike@gmail.com>
2025-01-17 20:41:21 -0700

    add trie search algorithm

--- trie_main.py
+++ trie_main.py
@@ -21,6 +21,16 @@ def print_trie(node, prefix=""):
             print_trie(child_node, prefix + char)
 
 
+def trie_search(root, string):
+    node = root
+    for char in string:
+        if char not in node.children:
+            return None
+        node = node.children[char]
+
+    if 0 in node.children:
+        return node.children[0]
+    return None
 
 
 trie_root = TrieNode()
@@ -32,3 +42,12 @@ trie_insert(trie_root, "hell on earth")
 
 # Print all words in the Trie
 print_trie(trie_root)
+
+search1 = trie_search(trie_root, "hello")
+search2 = trie_search(trie_root, "hello there")
+search3 = trie_search(trie_root, "hel there world")
+search4 = trie_search(trie_root, "hel")
+print(f"Search result for 'hello': {search1}")
+print(f"Search result for 'hello there': {search2}")
+print(f"Search result for 'hel there world': {search3}")
+print(f"Search result for 'hel': {search4}")
